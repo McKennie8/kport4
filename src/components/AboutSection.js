8e09a70767d5ef0ced0e077e0916159ce13e972a
Kirby <110064066+McKennie8@users.noreply.github.com>
2022-12-19 18:55:52 -0500

    1. Initial Commit

---
+++ src/components/AboutSection.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Kirby from "../img/Kirby.jpg";
+//Styled
+import { About, Description, Image, Hide } from "../styles";
+//Framer Motion
+import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
+//Fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+//Styled Components
+import styled from "styled-components";
+
+const AboutSection = () => {
+  return (
+    <About>
+      <Description>
+        <motion.div className="title">
+          <Hide>
+            <motion.h2 variants={titleAnim}>I work to make</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>
+              your <span>dreams</span> come
+            </motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
+          </Hide>
+        </motion.div>
+        <motion.p variants={fade}>
+          Contact me for your web development needs
+        </motion.p>
+        <ButtonDecor href="mailto:kirby@kirbymartin.com?subject=Friendly Inquiry&body=Hello Kirby,%0D%0A%0D%0AI am interested in learning more about your services.%0D%0A%0D%0ARegards,%0D%0A[Your Name]">
+          <ContactButton className="contact-button" variants={fade}>
+            Contact Me
+            <StyledEnvelope icon={faEnvelope} />
+          </ContactButton>
+        </ButtonDecor>
+      </Description>
+      <Image>
+        <motion.img variants={photoAnim} src={Kirby} alt="Kirby" />
+      </Image>
+      <Wave />
+    </About>
+  );
+};
+
+//Styled Components
+const StyledEnvelope = styled(FontAwesomeIcon)`
+  color: #ffa600;
+  font-size: 36px;
+  .contact-button:hover & {
+    color: white;
+  }
+`;
+
+const ContactButton = styled(motion.button)`
+  background-color: white;
+  color: #ffa600;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 20rem;
+  font-size: 2rem;
+  text-decoration: none;
+  cursor: pointer;
+  padding: 0rem 0rem;
+`;
+
+const ButtonDecor = styled(motion.a)`
+  text-decoration: none;
+`;
+
+export default AboutSection;
---
+++ src/components/FaqSection.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./UseScroll";
+import { fade } from "../animation";
+
+const FaqSection = () => {
+  const [element, controls] = useScroll();
+  return (
+    <Faq variants={fade} initial="hidden" animate={controls} ref={element}>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
+          <div className="answer">
+            <p>11111111111111.</p>
+            <p>AAAAAAAAAAAA.</p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>22222222222222.</p>
+            <p>BBBBBBBBBBBB.</p>
+          </div>
+        </Toggle>
+        <Toggle title="Diferrent Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What Products do you offer?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+    </Faq>
+  );
+};
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    font-size: 2rem;
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+      color: #e293de;
+    }
+  }
+`;
+
+export default FaqSection;
