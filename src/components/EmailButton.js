f445a9a40b5b57e4d0584e1f85a3a920b4f7cdd2
Kirby <110064066+McKennie8@users.noreply.github.com>
2022-12-19 18:52:26 -0500

    1. Initial Commit

---
+++ src/components/EmailButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmailButton = () => {
+  return (
+    <a href="mailto:info@example.com">
+      <button>Send Email</button>
+    </a>
+  );
+};
+
+export default EmailButton;
