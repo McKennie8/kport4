02a7f7760f8e6be516a20aa78ff3534943355412
Kirby <110064066+McKennie8@users.noreply.github.com>
2022-12-21 15:57:05 -0500

    7. FAQ Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -11,37 +11,47 @@ const FaqSection = () => {
   return (
     <Faq variants={fade} initial="hidden" animate={controls} ref={element}>
       <h2>
-        Any Questions <span>FAQ</span>
+        Any Questions? <span>FAQ</span>
       </h2>
       <AnimateSharedLayout>
-        <Toggle title="How Do I Start?">
+        <Toggle title="How Are Your Communication Skills?">
           <div className="answer">
-            <p>11111111111111.</p>
-            <p>AAAAAAAAAAAA.</p>
-          </div>
-        </Toggle>
-        <Toggle title="Daily Schedule">
-          <div className="answer">
-            <p>22222222222222.</p>
-            <p>BBBBBBBBBBBB.</p>
-          </div>
-        </Toggle>
-        <Toggle title="Diferrent Payment Methods">
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Necessitatibus, neque.
+              Top notch. I ran my own company for 15 years and learned how to
+              effectively and efficiently communicate with clients. I also have
+              a degree in Communication.
+            </p>
+            <p>
+              Please see "My Work" for walk-through videos which show off my
+              work and my people skills.
             </p>
           </div>
         </Toggle>
-        <Toggle title="What Products do you offer?">
+        <Toggle title="Can You Create Apps That Are Both Fast and Beautiful?">
           <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Necessitatibus, neque.
+              Absolutely. I create most of my apps using React which produces
+              incredibly fast apps.
             </p>
+          </div>
+        </Toggle>
+        <Toggle title="Where Are You From?">
+          <div className="answer">
+            <p>
+              I grew up in Kettering, Ohio. I've also lived in New York City,
+              San Francisco, Los Angeles, Virginia, and El Salvador.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What Are Your Passions Aside From Web Development?">
+          <div className="answer">
+            <p>I love to surf and play tennis.</p>
+            <p>I really enjoy playing and teaching all kinds of sports. </p>
+          </div>
+        </Toggle>
+        <Toggle title="Do You Have A Sense Of Humor?">
+          <div className="answer">
+            <p>Does the Pope wear a funny hat?</p>
           </div>
         </Toggle>
       </AnimateSharedLayout>
@@ -73,7 +83,7 @@ const Faq = styled(About)`
     padding: 2rem 0rem;
     p {
       padding: 1rem 0rem;
-      color: #e293de;
+      color: #ffba66;
     }
   }
 `;
