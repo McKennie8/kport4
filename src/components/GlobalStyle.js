d18d0c6c5545459839edfd0730e92fa818cc0045
Kirby <110064066+McKennie8@users.noreply.github.com>
2022-12-23 12:29:11 -0500

    12.8 More responsive styling

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -30,7 +30,7 @@ button{
     background: transparent;
     color: white;
     transition: all 0.5s ease;
-    font-family: 'Manrope', sans-serif;
+    font-family: 'Poppins', sans-serif;
     &:hover{
         background-color: white;
         color: #4e8183;
@@ -62,6 +62,51 @@ button{
         color: #ccc;
         font-size: 1.4rem;
         line-height: 150%;
+    }
+
+    @media (max-width: 480px) {
+        h2{
+        font-weight: lighter;
+        font-size: 2rem;
+    }
+    h3{
+        color: white;
+    }
+    h4{
+        font-weight: bold;
+       
+    }
+    h5 {
+        padding-top: 20rem;
+    }
+    a{
+        font-size: 1.1rem;
+    }
+    span{
+        font-weight: bold;
+        color:#4e8183
+    }
+    p{
+        padding: 3rem 0rem;
+        color: #ccc;
+        font-size: 1.1rem;
+        line-height: 150%;
+    }
+    button{
+    font-weight: bold;
+    font-size: 1.1.rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #4e8183;
+    background: transparent;
+    color: white;
+    transition: all 0.5s ease;
+    font-family: 'Poppins', sans-serif;
+    &:hover{
+        background-color: white;
+        color: #4e8183;
+    }
+}
     }
 
 `;
